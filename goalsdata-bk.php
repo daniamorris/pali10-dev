15ac54e6bda43cfe4c568fd0042ec811d7de8dec
daniamorris <daniamorris@gmail.com>
2014-05-14 07:43:56 -0700

    d
    d

--- goalsdata-bk.php
+++ goalsdata-bk.php
@@ -1,3 +1,71 @@
+<?php
+/*
+error_reporting(E_ALL);
+echo $_GET['uuid'];
+echo $_GET['myname'];
+echo $_GET['goals'];
+echo $_GET['notes'];
+echo $_GET['submit'];
+*/
+$goals = $_GET['goals'];
+$notes = $_GET['notes'];
+$stars = $_GET['stars'];
+$milage = $_GET['milage'];
+$timeframe = $_GET['timeframe'];
+$date = $_GET['date'];
+$location = $_GET['location'];
+$details = $_GET['details'];
+$moreinfo = $_GET['moreinfo'];
+
+include "dbcred.php";
+$db = mysql_connect("localhost", $user, $password);
+mysql_select_db($db_name,$db);
+
+if (isset($_GET['myname']) && $_GET['myname'] == 'test') {
+    $query = mysql_query("SELECT * FROM scoreboard WHERE myname='Dania Morris'", $db);
+    if (mysql_affected_rows() > 0) {
+        while ($row = mysql_fetch_assoc($query)) {
+            echo "<div>" . $row['myname'] . "</div>";
+        }
+    } else {
+        echo "No Data Found";
+    }
+}
+
+if (isset($_GET['submit']) && $_GET['submit'] == 'insertgoals') {
+    $query = mysql_query("INSERT INTO scoreboard (id,goals,notes) VALUES (NULL, '$goals', '$notes')", $db);
+    if (mysql_affected_rows() > 0) {
+        while ($row = mysql_fetch_assoc($query)) {
+            echo "<div>" . $row['goals'] . "</div>";
+        }
+    } else {
+        echo "No Data Found";
+    }
+}
+
+if (isset($_GET['submit']) && $_GET['submit'] == 'logtrain') {
+    $query = mysql_query("INSERT INTO scoreboard (id,stars,milage,timeframe) VALUES (NULL, '$stars', '$milage','$timeframe')", $db);
+    if (mysql_affected_rows() > 0) {
+        while ($row = mysql_fetch_assoc($query)) {
+            echo "<div>" . $row['milage'] . "</div>";
+        }
+    } else {
+        echo "No Data Found";
+    }
+}
+
+if (isset($_GET['submit']) && $_GET['submit'] == 'trainevent') {
+    $query = mysql_query("INSERT INTO events (id,date,location,details,moreinfo) VALUES (NULL, '$date', '$location', '$details','$moreinfo')", $db);
+    if (mysql_affected_rows() > 0) {
+        while ($row = mysql_fetch_assoc($query)) {
+            echo "<div>" . $row['moreinfo'] . "</div>";
+        }
+    } else {
+        echo "No Data Found";
+    }
+}
+
+?>
 <!DOCTYPE html>
 <html>
     <head>
