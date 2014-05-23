f921e5b96334bc20792c9ff707d60231636c09b4
daniamorris <daniamorris@gmail.com>
2014-05-23 14:25:16 -0700

    var
    var

--- scoreboard2.php
+++ scoreboard2.php
@@ -2,7 +2,8 @@
 error_reporting(E_ALL);
 include "dbcred.php";
 include "DDMmobil.class.php";
-$myuuid = 'b710c14d6a29be70';
+//$myuuid = 'b710c14d6a29be70';
+$myuuid = $_GET['myuuid'];
 try {
     $dbh = new PDO($dsn, $user, $password);
 } catch (PDOException $e) {
