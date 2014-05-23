151d8c064124c0eefdc41777d19ae063c1fcdd17
daniamorris <daniamorris@gmail.com>
2014-05-23 13:51:29 -0700

    pdo
    pdo

---
+++ DDMmobil.class.php
@@ -0,0 +1,42 @@
+﻿<?php
+class DDMmobil
+{
+  /**
+   * Instance of database connection class
+   * @access protected
+   * @var PDO object
+   */
+  protected $db;
+	
+  function __construct(PDO $db)
+  {
+    $this->db       = $db;
+  }
+
+   public function scoreboard($myuuid)
+  {
+
+    try
+    {
+      $sql = "SELECT * FROM scoreboard WHERE uuid=:myuuid";
+      $stmt = $this->db->prepare($sql);
+      $stmt->bindParam(':myuuid', $myuuid);
+      $stmt->execute();
+		while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+		printf("<p>Stars: %s</p><p>milage: %s</p><p>goals: %s</p><p>Notes: %s</p>", $row["stars"], $row["milage"], $row["goals"], $row["notes"]);
+		}
+    }
+    
+	catch (PDOException $e)
+	{
+  	echo 'PDO Exception Caught.  ';
+  	echo 'Error with the database: <br />';
+  	echo 'SQL Query: ', $sql;
+  	echo 'Error: ' . $e->getMessage();
+	}
+	echo '<div class="clear"> </div>';
+
+  }
+}
+
+?>
