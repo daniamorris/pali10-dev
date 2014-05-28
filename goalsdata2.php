895a9c9c92d28930cb1b0cfee68db046ad58bfc4
daniamorris <daniamorris@gmail.com>
2014-05-27 21:35:04 -0700

    d
    d

--- goalsdata2.php
+++ goalsdata2.php
@@ -2,8 +2,8 @@
 error_reporting(E_ALL);
 include "dbcred.php";
 include "DDMmobil.class.php";
-$myuuid = 'b710c14d6a29be70';
-//$myuuid = $_GET['myuuid'];
+//$myuuid = 'b710c14d6a29be70';
+$myuuid = $_GET['myuuid'];
 $goals = $_GET['goals'];
 $notes = $_GET['notes'];
 $hours = $_GET['hours'];
