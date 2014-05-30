ca9eaa732478b81348e63c4a8a2a54c8bb08737a
daniamorris <daniamorris@gmail.com>
2014-05-29 20:23:05 -0700

    d
    d

--- DDMmobil.class.php
+++ DDMmobil.class.php
@@ -104,7 +104,7 @@ class DDMmobil
       $stmt->bindParam(':notes', $notes);
       $stmt->execute();
 		echo '<div class="container"><h3 class="center-block">Your goal has been added!</h3></div>';
-		include 'goals.html';
+		//include 'goals.html';
     }
     
 	catch (PDOException $e)
@@ -214,7 +214,7 @@ class DDMmobil
       $stmt->bindParam(':stars', $stars);
       $stmt->execute();
 		echo '<div class="container"><h3 class="center-block">Your training has been added!</h3></div>';
-		include 'log.html';
+		//include 'log.html';
     }
     
 	catch (PDOException $e)
@@ -243,7 +243,7 @@ class DDMmobil
       $stmt->execute();
 		echo '<div class="container"><h3 class="center-block">Your training event has been added!</h3></div>';
 		//echo $myuuid;
-		include 'train.html';
+		//include 'train.html';
     }
     
 	catch (PDOException $e)
